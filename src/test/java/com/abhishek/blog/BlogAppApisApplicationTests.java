f0dfc75c98185697ad94acbc14111a7bd501a873
Abhishek <okabhi95@gmail.com>
2022-12-31 16:58:24 +0530

    all client requrirements are gathred and now lets start our setup

---
+++ src/test/java/com/abhishek/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
